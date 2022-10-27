acdf6b21b55ec4e55f58c64893bfe2e701bd96d2
mathildamus <113375599+MariamMathildaAmonashvili@users.noreply.github.com>
2022-10-27 16:22:43 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+
+// Task 1 //
+
+const arrayNum = [ 4,6,7,8,9] ;
+
+// console.log (arrayNum); 
+
+const summary = arrayNum [0] + arrayNum[1] + arrayNum[2] + arrayNum [3] + arrayNum[4] ;
+const sum = arrayNum.length;
+const result = summary / sum ;
+
+
+//console.log(result) 
+
+//Task 2 //
+
+
+
+const object1 = {
+    Name : "Mariami" , 
+    Age : 21 , 
+} ;
+
+const object2 = {
+    Name : "Mathilda" ,
+    Age : 27 ,
+} ;
+
+const object3 = {
+    Name : "Elene" ,
+    Age : 6 ,
+} ; 
+
+const object4 = {
+    Name : "Giorgi" , 
+    Age : 46,
+} ; 
+
+const object5 = {
+    Name : "Giga" , 
+    Age : 34 ,
+} ;
+ 
+const objectResult = [object1 , object2 , object3 , object4 , object5] ;
+
+//console.log(objectResult) 
+
+
+//Task 3 //
+const myArr = [ 579237932] ;
+const info = {
+    firstName : "Mariam Mathilda", 
+    lastName : "Amonashvili",
+    age : 21 , 
+    phoneNumbers : myArr ,
+    address : "Tbilisi"  ,
+    zipCode : 0162 ,
+
+} ;
+
+console.log("User name is "+info.firstName +", user age is "+ info.age +" , user address is " +info.address);
